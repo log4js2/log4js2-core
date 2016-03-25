d5b563887174fa4aed209da4f305583bc9093b76
Robin Schultz <developer@cunae.com>
2016-03-25 14:37:05 -0400

    Add tests

---
+++ test/node-test.js
@@ -0,0 +1,32 @@
+var log4js = require('../dist/log4js2');
+
+log4js.configure({
+    tagLayout : '%d{MM-dd-yyyy HH:mm:ss,S} [%level] %logger:%M:%line|%message',
+    appenders : [ 'consoleAppender' ],
+    loggers : [ {
+	    logLevel : log4js.LogLevel.INFO
+    } ],
+    allowAppenderInjection : true
+});
+
+var log = log4js.getLogger();
+
+function namedFunctionLogging() {
+	
+	log.trace('This is a named trace message');
+	log.debug('This is a named debug message');
+	log.info('This is a named info message');
+	log.warn('This is a named warn message');
+	log.error('This is a named error message');
+	
+}
+
+namedFunctionLogging();
+
+log.trace('This is a unnamed trace message');
+log.debug('This is a unnamed debug message');
+log.info('This is a unnamed info message');
+log.warn('This is a unnamed warn message');
+log.error('This is a unnamed error message');
+
+log.info('This is a test log with {}', 'parameters');
